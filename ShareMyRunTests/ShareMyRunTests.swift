cdaa8b26907e72c900605f4011c3fa74bdf4893f
Camden Webster <59626059+camdenwebster@users.noreply.github.com>
2026-01-18 19:11:51 -0600

    Initial Commit

---
+++ ShareMyRunTests/ShareMyRunTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShareMyRunTests.swift
+//  ShareMyRunTests
+//
+//  Created by Camden Webster on 1/18/26.
+//
+
+import Testing
+@testable import ShareMyRun
+
+struct ShareMyRunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
